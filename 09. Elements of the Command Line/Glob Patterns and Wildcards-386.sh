f678375269a8162b1f22d564fbd67f04c2c82000
KH <kjholla@gmail.com>
2019-12-05 06:56:13 -0600

    Completed Mission: Glob Patterns and Wildcards

---
+++ 09. Elements of the Command Line/Glob Patterns and Wildcards-386.sh
@@ -0,0 +1,23 @@
+## 2. The * Wildcard ##
+
+/home/dq/brats$ ls
+
+## 3. The ? Wildcard ##
+
+/home/dq/brats$ ls ????
+
+## 4. Escaping Characters ##
+
+/home/dq/brats$ ls t\? a\*
+
+## 5. The Wildcard [] ##
+
+/home/dq/brats$ ls *[!a-z]
+
+## 6. Other Wildcards ##
+
+/home/dq/brats$ ls *[[:alnum:]]
+
+## 7. Summary and Practice ##
+
+/home/dq/practice/wildcards$ ls
