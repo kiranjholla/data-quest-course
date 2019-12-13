314b8dcc8cb58e8702a6ed8b778a2caf4044b6a8
KH <kjholla@gmail.com>
2019-12-13 07:39:38 -0600

    Completed Mission: Subqueries

---
+++ 11. SQL Fundamentals/Subqueries-255.sql
@@ -0,0 +1,19 @@
+## 2. Subqueries ##
+
+SELECT Major, Unemployment_rate FROM recent_grads WHERE Unemployment_rate < (SELECT AVG(Unemployment_rate) FROM recent_grads) ORDER BY Unemployment_rate 
+
+## 3. Subquery In SELECT ##
+
+SELECT COUNT(*) * 1.0 / (SELECT COUNT(*) FROM recent_grads) AS proportion_abv_avg FROM recent_grads WHERE ShareWomen > (SELECT AVG(ShareWomen) FROM recent_grads)
+
+## 4. Returning Multiple Results In Subqueries ##
+
+SELECT Major, Major_category FROM recent_grads WHERE Major_category IN (SELECT Major_category FROM recent_grads GROUP BY Major_category ORDER BY TOTAL(Total) DESC LIMIT 5)
+
+## 5. Building Complex Subqueries ##
+
+SELECT AVG(CAST(Sample_size as float) / CAST(Total as float)) as avg_ratio FROM recent_grads
+
+## 6. Practice Integrating A Subquery With The Outer Query ##
+
+SELECT Major, Major_category, CAST(Sample_size as float) / CAST(Total as float) as ratio FROM recent_grads WHERE ratio > (SELECT AVG(CAST(Sample_size as float) / CAST(Total as float)) as avg_ratio FROM recent_grads)
